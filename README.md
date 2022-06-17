46aca8d497cbeb2c5bed949a6b942a5515fb5a3e
dariusmaverick <absolutoinutil@gmail.com>
2022-06-17 12:58:38 -0300

    add

---
+++ README.md
@@ -0,0 +1,3 @@
+# nvim
+
+my text editor configuration
---
+++ lua/user/keymaps.lua
@@ -0,0 +1,34 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
+keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers
+keymap("n", "<Leader>w", ":bnext<CR>", opts)
+keymap("n", "<Leader>q", ":bprevious<CR>", opts)
+
+-- Insert --
+-- Press jk fast to enter
+keymap("i", "jk", "<ESC>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+keymap('t', "<ESC>", "<C-\\><C-n>", term_opts)
